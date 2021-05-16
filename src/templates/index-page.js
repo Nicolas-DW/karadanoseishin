c47eec82e0599e0f17f23698fbdbeb6ba3adcff1
Nicolas-DW <39828173+Nicolas-DW@users.noreply.github.com>
2021-05-16 23:34:33 +0200

    Update index-page.js

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -36,7 +36,7 @@ export const pageQuery = graphql`
         tagline
         featuredImage {
           childImageSharp {
-            gatsbyImageData(layout: CONSTRAINED, transformOptions: {fit: "cover", cropFocus: "attention"}, width: 585, height: 439)
+            gatsbyImageData(layout: CONSTRAINED, transformOptions: {fit: "COVER", cropFocus: "ATTENTION"}, width: 585, height: 439)
           }
         }
         cta {
